ff8574b23d45313695935151ef36be032e616296
glennodonovan <odonovan.glenn@gmail.com>
2014-11-08 17:00:41 +0000

    Commit Basic app

--- Applicaiton.py
+++ Applicaiton.py
@@ -1,43 +1,56 @@
 
-import boto.ec2
+import boto
 import UtilityClasses
 print "Hello Welcome to AWSManager"
 
+selectRegion =  "eu-west-1"
 
-response = raw_input("How would you like to enter your amazon credentials? select 1 for enter manually select 2 for read from file: ")
+
+
+response = raw_input('''Please select What feature you would like to view:
+  \n 1 for amazon EC 2 instances
+  \n 2 for S3 Bucket listings
+  \n 3 Enable cloud watch for ''')
 
 
 for case in UtilityClasses.switch(response):
     if case('1'):
-        selectRegion =  raw_input("Please select your region: ")
-        accessKey = raw_input("Please enter your access key: ")
-        secretKey = raw_input("Please provide your secret key: ")
+
+        print("Running AWS EC2 Instances")
+        conn = boto.ec2.connect_to_region(selectRegion)
+
+        #Build an array called Instances and append each instance this via the extend method
+        instances = []
+        reservations = conn.get_all_reservations()
+        for reservation in reservations:
+            instances.extend(reservation.instances)
+#Print out the information for each instance
+        for i in instances:
+            print ("Instance ID:",i ,
+                    "Instance Type:", i.instance_type,
+                    "Instance Region: ", i.placement)
+
+
 
 
         break
     if case('2'):
-        print "Red from file"
+        print("You selected S3 buckets")
+
+        print("Current S3 Buckets")
 
 
-        awsKeyFile = raw_input("Please enter the location of your AWS key file which you downloaded: ")
-        f = open(awsKeyFile, 'r')
+        conn = boto.connect_s3()
+        buckets = conn.get_all_buckets()
 
-        accessKey = f.readline()[15:]
-        secretKey = f.readline()[13:]
-        selectRegion = raw_input("Please enter the region you wish to conenct to: ")
-
-        conn = boto.ec2.connect_to_region(selectRegion,
-        aws_access_key_id=accessKey,
-        aws_secret_access_key=secretKey)
+        if not buckets:
+            print("No buckets exists")
+        else:
+            for bucket in buckets:
+                print bucket
 
 
-
-
-
-
-        break
-    if case(): # default, could also just omit condition or 'if True'
-        print "Boom!"
+#Conenct to EC2 and display current instances of EC 2
 
 
 
@@ -47,10 +60,6 @@ for case in UtilityClasses.switch(response):
 
 
 
-
-reservations = conn.get_all_reservations()
-for reservation in reservations:
-    print reservation.instances
 
 
 
